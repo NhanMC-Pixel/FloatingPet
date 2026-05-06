8256629d153c176b71864406e20b41fad75ac8df
NhanMC-Pixel <hieunhan6292@gmail.com>
2026-05-06 12:48:03 +0700

    Fix build.gradle.kts files

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,19 +1,41 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.floatingpet.data"
+    compileSdk = 34
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
 dependencies {
     implementation(project(":domain"))
     implementation(project(":core"))
 
-    // Room
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     kapt("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
 
-    // Hilt
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
     implementation("androidx.hilt:hilt-common:1.1.0")
     kapt("androidx.hilt:hilt-compiler:1.1.0")
 
-    // Kotlin
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+}
+
+kapt {
+    correctErrorTypes = true
 }
